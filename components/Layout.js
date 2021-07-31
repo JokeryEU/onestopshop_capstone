43e3bae5736db4b3c0b6df16d2c9a9140ee82df4
Mihai Ivanov <mikee2988@yahoo.com>
2021-07-31 22:11:03 +0300

    added styles to the layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,22 @@
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
 import Head from 'next/head'
+import useStyles from '../utils/styles'
 
 const Layout = ({ children }) => {
+  const classes = useStyles()
+
   return (
     <>
       <Head>
         <title>Welcome to OneStopShop</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" classname={classes.navbar}>
         <Toolbar>
           <Typography>OneStopShop</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container classname={classes.main}>{children}</Container>
+      <footer classname={classes.footer}>
         <Typography>All rights reserved. OneStopShop.</Typography>
       </footer>
     </>
