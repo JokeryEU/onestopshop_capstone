f6c02e8bd7a87ae14d3f2b5ce1eec7c89ce5f185
Mihai Ivanov <mikee2988@yahoo.com>
2021-08-01 01:07:41 +0300

    added styling to h1 and color to the links and buttons and using fonts Roboto

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,41 @@
-import { AppBar, Container, Link, Toolbar, Typography } from '@material-ui/core'
+import {
+  AppBar,
+  Container,
+  createTheme,
+  CssBaseline,
+  Link,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
 import Head from 'next/head'
 import useStyles from '../utils/styles'
 import NextLink from 'next/link'
 
 const Layout = ({ description, title, children }) => {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: '400',
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: '400',
+        margin: '1rem 0',
+      },
+    },
+    pallete: {
+      type: 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  })
   const classes = useStyles()
 
   return (
@@ -14,28 +46,31 @@ const Layout = ({ description, title, children }) => {
         </title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}>OneStopShop</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>OneStopShop</Typography>
+              </Link>
             </NextLink>
-            <NextLink href="/login" passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>All rights reserved. OneStopShop.</Typography>
-      </footer>
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. OneStopShop.</Typography>
+        </footer>
+      </ThemeProvider>
     </>
   )
 }
