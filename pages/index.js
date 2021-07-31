20a18fbcf431b9e1162095048b94f4eeb9a27f74
Mihai Ivanov <mikee2988@yahoo.com>
2021-07-31 23:08:17 +0300

    first iteration of working product cards
    this is a test to see if it was working, will create a component for it later

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,43 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@material-ui/core'
 import Layout from '../components/layout'
 
 export default function Home() {
   return (
     <Layout>
       <h1>Products</h1>
+      <Grid container spacing={3}>
+        {toBeAddedFromDB.products.map((product) => (
+          <Grid item md={4} key={product._id}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  image={product.image}
+                  title={product.name}
+                ></CardMedia>
+                <CardContent>
+                  <Typography>{product.name}</Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Typography>€{product.price}</Typography>
+                <Button size="small" color="primary">
+                  Add to cart
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </Layout>
   )
 }
