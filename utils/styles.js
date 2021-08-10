4e28ffb0c653e1725476e351dc0ae325e4dc96d7
Mihai Ivanov <mikee2988@yahoo.com>
2021-08-10 07:26:35 +0300

    added search page and filters

--- utils/styles.js
+++ utils/styles.js
@@ -65,6 +65,7 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'space-between',
   },
   menuButton: { padding: 0 },
+  mt1: { marginTop: '1rem' },
   // search
   searchSection: {
     display: 'none',
@@ -91,6 +92,9 @@ const useStyles = makeStyles((theme) => ({
     '& span': {
       color: '#000000',
     },
+  },
+  sort: {
+    marginRight: 5,
   },
 }))
 
