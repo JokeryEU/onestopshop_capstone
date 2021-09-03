db6935d2fb8f34543476e5638a99eb0f989d3257
Mihai Ivanov <mikee2988@yahoo.com>
2021-09-03 12:55:35 +0300

    updated packages and db
    removed console logs and removed unsupported mongo options with the new version

--- utils/db.js
+++ utils/db.js
@@ -9,7 +9,6 @@ async function connect() {
   if (mongoose.connections.length > 0) {
     connection.isConnected = mongoose.connections[0].readyState
     if (connection.isConnected === 1) {
-      console.log('use previous connection')
       return
     }
     await mongoose.disconnect()
@@ -17,8 +16,6 @@ async function connect() {
   const db = await mongoose.connect(process.env.MONGODB_ADDRESS, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
   })
   connection.isConnected = db.connections[0].readyState
 }
@@ -28,8 +25,6 @@ async function disconnect() {
     if (process.env.MONGODB_ADDRESS === 'production') {
       await mongoose.disconnect()
       connection.isConnected = false
-    } else {
-      console.log('Not disconnect')
     }
   }
 }
