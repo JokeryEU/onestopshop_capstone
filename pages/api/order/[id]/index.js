c7fe315e6d3f9b23b5f6eda67df1160fb7e3488e
Mihai Ivanov <mikee2988@yahoo.com>
2021-09-30 03:44:09 +0300

    added delete orders as admin route

--- pages/api/order/[id]/index.js
+++ pages/api/order/[id]/index.js
@@ -1,16 +1,30 @@
 import nc from 'next-connect'
 import Order from '../../../../models/Order'
-import { isAuth } from '../../../../utils/auth'
+import { isAuth, isAdmin } from '../../../../utils/auth'
 import db from '../../../../utils/db'
 
 const handler = nc()
-handler.use(isAuth)
 
-handler.get(async (req, res) => {
-  await db.connect()
-  const order = await Order.findById(req.query.id)
-  await db.disconnect()
-  res.send(order)
-})
+handler
+  .use(isAuth)
+  .get(async (req, res) => {
+    await db.connect()
+    const order = await Order.findById(req.query.id)
+    await db.disconnect()
+    res.send(order)
+  })
+  .use(isAuth, isAdmin)
+  .delete(async (req, res) => {
+    await db.connect()
+    const order = await Order.findById(req.query.id)
+    if (order) {
+      const deletedOrder = await order.remove()
+      await db.disconnect()
+      res.send({ message: 'Order Deleted', order: deletedOrder })
+    } else {
+      await db.disconnect()
+      res.status(404).send({ message: 'Order Not Found' })
+    }
+  })
 
 export default handler
